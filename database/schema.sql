b469cfe92a806802f4638b1fa4330631108de6e8
Jane <noiorhyun@gmail.com>
2025-05-06 18:26:19 -0700

    add students and courses

--- database/schema.sql
+++ database/schema.sql
@@ -26,16 +26,25 @@ CREATE TABLE registrations (
 -- insert data for students table
 INSERT INTO students (name, email) VALUES
 ('Alice Smith', 'alice.smith@example.com'),
-('Bob Johnson', 'bob.johnson@example.com');
+('Bob Johnson', 'bob.johnson@example.com'),
+('Jane Wang', 'jane.wang@example.com'),
+('May Du', 'may.du@example.com');
 
 -- insert data for courses table
 INSERT INTO courses (course_name, section, capacity) VALUES
 ('Introduction to Programming', 'A', 1),  -- Capacity set to 1
 ('Introduction to Programming', 'B', 1),  -- Capacity set to 1
 ('Database Management', 'C', 1),      -- Capacity set to 1
-('Database Management', 'D', 1);      -- Capacity set to 1
+('Database Management', 'D', 1),      -- Capacity set to 1
+('Advanced Web Programming', 'A', 1),      -- Capacity set to 1
+('Advanced Web Programming', 'B', 1); -- Capacity set to 1 
 
 -- insert data for registrations table
 INSERT INTO registrations (student_id, course_id) VALUES
 (1, 1), -- Alice registered Introduction to Programming A
-(2, 4); -- Bob registered Database Management D
+(2, 4), -- Bob registered Database Management D
+(3, 2), -- Jane registered Database Management C
+(4, 3); -- May registered Advanced Web Programming A
+
+
+
